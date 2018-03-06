6de6495c9299890da4b1114b5531e90dd572faac
Yancey_Chan <chenyuanshan@outlook1120.com>
2018-03-06 10:25:15 +0800

    init pod

---
+++ MVVMDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  MVVMDemo
+//
+//  Created by PSBC_Yancey on 2018/3/6.
+//  Copyright © 2018年 Yancey_Chan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+use_frameworks!
+inhibit_all_warnings!
+target 'MVVMDemo' do
+pod 'ReactiveObjC', '~> 2.1.0'
+pod 'Masonry'
+pod 'IQKeyboardManager'
+end
